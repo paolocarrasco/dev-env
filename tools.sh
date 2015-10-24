2698b7c4ef87755eb457c54b9e487054ea0c32dc
Paolo Carrasco <paolocarrasco@users.noreply.github.com>
2015-10-24 14:56:14 -0500

    script installs the tools needed to develop

---
+++ tools.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+sudo apt-get update
+
+sudo apt-get install --yes xclip
+sudo apt-get install --yes wget
+sudo apt-get install --yes vim
+sudo apt-get install --yes git
+sudo apt-get install --yes virtualbox
+sudo apt-get install --yes vagrant
+sudo apt-get install --yes virtualbox-dkms
+
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+# Docker
+apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo deb https://apt.dockerproject.org/repo ubuntu-precise main > ~/docker.list
+sudo cp ~/docker.list /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+apt-get purge lxc-docker*
+apt-cache policy docker-engine
+sudo apt-get update
+sudo apt-get install docker-engine
+
