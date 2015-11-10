7381aad431f3326ce71f3df2e2e3ae02bd417a3e
Paolo Carrasco <paolo.carrasco@gmail.com>
2015-11-09 23:33:10 -0500

    databases script installs mongo, mysql & psql

---
+++ databases.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+# MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install --yes mongodb-org
+
+# PostgreSql
+sudo apt-get install --yes postgresql-9.4
+
+# MySql
+sudo apt-get install --yes mysql-server mysql-client
